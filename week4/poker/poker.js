52da2bf7a54307c96a312d3f11dad452bf071787
agz567 <agonzalez2017@kellogg.northwestern.edu>
2016-10-21 00:15:10 -0500

    Add files via upload

---
+++ week4/poker/poker.js
@@ -0,0 +1,39 @@
+// shuffles (randomizes an array)
+// e.g. myArray.shuffle()
+Array.prototype.shuffle = function() {
+  var currentIndex = this.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = this[currentIndex];
+    this[currentIndex] = this[randomIndex];
+    this[randomIndex] = temporaryValue;
+  }
+  return this;
+}
+
+// returns a deck of cards
+// e.g. getDeck()
+window.getDeck = function() {
+  var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'];
+  var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+  var cards = [];
+  ranks.forEach(function(rank, index) {
+    suits.forEach(function(suit, index) {
+      cards.push(rank + "_of_" + suit);
+    });
+  });
+  return cards;
+}
+
+$(function stackDeal(5) {
+
+    if (this.cards.length > 0)
+      return this.cards.shift();
+    else
+      return null;
+  }
+
+
+  // Your code goes here!
+})
